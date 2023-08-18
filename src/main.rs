312ce4ce002d9887013c569e08e7ea4a3f2c17e2
Accalia <Accalia@Elementia.me>
2023-08-18 19:26:17 -0400

    parralelized

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 use std::vec::Vec;
 use std::time::SystemTime;
 use std::thread;
-use std::sync::{Arc, Mutex, mpsc};
+use std::sync::{Arc, Mutex};
 
 fn main() {
     let now = SystemTime::now();
@@ -16,88 +16,91 @@ fn main() {
     println!("{} valid wordle words", words.len());
     println!("{} ms to read file to map", elapsed.expect("should be ok").as_millis());
     let now = SystemTime::now();
-    let wordles = wordle_multiproc(words);
+    let wordles = wordle(words);
     let elapsed = now.elapsed();
     println!("{} valid wordle combos", wordles.len());
     println!("{} ms to wordle", elapsed.expect("should be ok").as_millis());
     let file = File::create("wordle.txt").expect("Error creating output file!");
     let mut file = LineWriter::new(file);
     for wordle in wordles {
-        file.write(wordle.as_bytes()).expect("Error writing line to output file!");
+        file.write(format!("{}\n", wordle).as_bytes()).expect("Error writing line to output file!");
     }
     println!("END");
 }
 
-fn wordle_multiproc (words: Vec<(u64, Vec<String>)>) -> Vec<String> {
-    let threadcount = 32;
+fn filter_words(key: u64, words: &[(u64, Vec<String>)]) -> Vec<(u64, Vec<String>)> {
+    let mut filtered: Vec<(u64, Vec<String>)> = Vec::new();
+    for (k, v) in words {
+        if k & key == 0 {
+            filtered.push((*k, v.to_vec()));
+        }
+    }
+    filtered
+}
+
+fn append_words (a: &Vec<String>, b: &Vec<String>) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    for a in a {
+        for b in b {
+            result.push(format!("{} {}", a, b));
+        }
+    }
+    result
+}
+
+fn wordle (words: Vec<(u64, Vec<String>)>) -> Vec<String> {
+    let now = SystemTime::now();
     let result: Vec<String> = Vec::new();
     let mutex = Arc::new(Mutex::new(result));
-    let (post, poll) = mpsc::channel();
+    let i = Arc::new(Mutex::new(0));
     let mut threads = Vec::new();
-    for i in 0..threadcount {
-        let post = post.clone();
+    for _ in 0..thread::available_parallelism().expect("should be able to read core count").get() {
         let mutex = Arc::clone(&mutex);
-        let threadcount = threadcount.clone();
         let words = words.clone();
         let i = i.clone();
+        let now = now.clone();
         threads.push(thread::spawn(move || {
-            let mut a = i;
-            while a < words.len() {
+            let mut a;
+            loop {
+                {
+                    let mut i = i.lock().unwrap();
+                    a = *i;
+                    *i += 1;
+                }
+                if a >= words.len() { break; }
                 let (key, aword) = &words[a];
-                for (b, (bkey, bword)) in words[(a+1)..].iter().enumerate() {
-                    if key & bkey != 0 { continue }
+                if a % 100 == 0 || a == words.len() - 1 {
+                    let elapsed = now.elapsed().expect("TIME ERROR");
+                    let seconds = elapsed.as_secs() % 60;
+                    let minutes = (elapsed.as_secs() / 60) % 60;
+                    let hours = (elapsed.as_secs() / 60) / 60;
+                    let percent = 100.0 * ( (a + 1) as f64 / words.len() as f64);
+                    println!("{} - {hours:0>2}:{minutes:0>2}:{seconds:0>2} - {percent:8.4}%", &aword[0]);
+                }
+                let innerwords = filter_words(*key, &words[(a+1)..]);
+                for (b, (bkey, bword)) in innerwords.iter().enumerate() {
                     let key = key | bkey;
-                    for (c, (ckey, cword)) in words[(a+b+1)..].iter().enumerate() {
-                        if key & ckey != 0 { continue }
+                    let innerwords = filter_words(key, &innerwords[(b+1)..]);
+                    for (c, (ckey, cword)) in innerwords.iter().enumerate() {
                         let key = key | ckey;
-                        for (d, (dkey, dword)) in words[(a+b+c+1)..].iter().enumerate() {
-                            if key & dkey != 0 { continue }
+                        let innerwords = filter_words(key, &innerwords[(c+1)..]);
+                        for (d, (dkey, dword)) in innerwords.iter().enumerate() {
                             let key = key | dkey;
-                            for (_e, (ekey, eword)) in words[(a+b+c+d+1)..].iter().enumerate() {
-                                if key & ekey != 0 { continue }
-                                for a in aword {
-                                    for b in bword {
-                                        for c in cword {
-                                            for d in dword {
-                                                for e in eword {
-                                                    let mut vec = mutex.lock().unwrap();
-                                                    vec.push(format!("{} {} {} {} {}\n", a, b, c, d, e));
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
+                            let innerwords = filter_words(key, &innerwords[(d+1)..]);
+                            for (_, (_, eword)) in innerwords.iter().enumerate() {
+                                let mut vec = mutex.lock().unwrap();
+                                vec.extend(append_words(aword, &append_words(bword, &append_words(cword, &append_words(dword, eword)))));
                             }
                         }
                     }
                 }
-                a = a + threadcount;
-                post.send(aword[0].clone()).unwrap();
             }
         }));
     }
-    let throbber = thread::spawn(move || {
-        let now = SystemTime::now();
-        let mut i = 0;
-        let len = words.len();
-        while i < len {
-            let word: String = poll.recv().unwrap();
-            i = i + 1;
-            if i % 100 == 0 || i == len {
-                let elapsed = now.elapsed().expect("TIME ERROR");
-                let seconds = elapsed.as_secs() % 60;
-                let minutes = (elapsed.as_secs() / 60) % 60;
-                let hours = (elapsed.as_secs() / 60) / 60;
-                let percent: f64 = ((i as f64) / words.len() as f64) * 100.0;
-                println!("{word} - {hours:0>2}:{minutes:0>2}:{seconds:0>2} - {percent:8.4}%");
-            }
-        }
-    });
 
     for thread in threads {
         thread.join().unwrap();
     }
-    throbber.join().unwrap();
 
     let x = (*mutex.lock().unwrap()).to_vec();
     x
@@ -126,6 +129,7 @@ where P: AsRef<Path>, {
     for (bits, vec) in map.iter() {
         words.push((*bits,vec.to_vec()));
     }
+    words.sort_by(|(_, a), (_, b) | a[0].cmp(&b[0]));
     words
 
 }
@@ -179,5 +183,6 @@ fn string_to_bitfield(s: &String) -> (bool, u64) {
         }
         result = result | char_to_bit(letter);
     }
-    (s.len() == 5 && count == 5, result)
+    let valid_wordle = s.len() == 5 && count == 5;
+    (valid_wordle, result)
 }
